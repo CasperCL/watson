1d0d08471150063aecd5352cbc35f98eca424953
--global <--global>
2018-01-25 19:14:25 +0100

    Removed histograms

--- visualise.py
+++ visualise.py
@@ -1,18 +1,10 @@
+import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 
 X = pandas.read_csv('users_products.gen.csv')
 
-# Hist
-user_order_count = X.groupby('user_id').apply(lambda x: sum(x['orders'] ))
-plt.hist(user_order_count, bins=100)
-plt.xlabel('users')
-plt.ylabel('orders')
-plt.show()
+a = X.groupby(['product_id']).agg({'orders': np.sum})
+top = a.sort_values(by='orders', ascending=False)[:10]
 
-# Hist
-product_order_count = X.groupby('product_id').apply(lambda x: sum(x['orders'] ))
-plt.hist(product_order_count, bins=100)
-plt.xlabel('products')
-plt.ylabel('orders')
-plt.show()
+print(top)
