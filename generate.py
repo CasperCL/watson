b1797d33f28bef6dfd0bf321229ea95b9a14b484
Casper Eekhof <caspereekhof@icloud.com>
2018-02-25 20:37:56 +0100

    Cleansed generate.py

--- generate.py
+++ generate.py
@@ -6,6 +6,13 @@ import enum
 import mimesis
 
 
+N_USERS = 1000
+N_PRODUCTS = 5000
+PRODUCTS_FILE = 'products.csv'
+USERS_FILE = 'users.gen.csv'
+BUY_HISTORY_FILE = 'users_products.gen.csv'
+
+
 class UserType(enum.Enum):
     BULK_INFREQUENT = 0
     BULK_FREQUENT = 1
@@ -42,50 +49,53 @@ class User:
                 order += [random.choice(products)] * random.randint(2, 10)
         return order
 
-N_USERS = 1000
-N_PRODUCTS = 5000
+def generate_users(limit: int):
+    person_generator = mimesis.Personal('nl')
+    users = []
+    for i in range(0, limit):
+        name = person_generator.full_name()
+        users.append(User(i, name))
+    return users
 
-person_generator = mimesis.Personal('nl')
+def read_products(products_file, limit=None):
+    products = []
+    valid_product_counter = 0
+    with open(products_file, encoding='latin-1') as file:
+        reader = csv.reader(file, delimiter=';')
+        header = next(reader)
+        for index, product_csv in enumerate(reader):
+            ean = product_csv[header.index('EANNummer')].strip()
+            name = product_csv[header.index('NaamRek')]
+            brand = product_csv[header.index('Merk')]
+            if not ean:
+                continue
+            products.append(Product(valid_product_counter, name, brand))
+            valid_product_counter += 1
+            if limit is not None and valid_product_counter >= limit:
+                break
+    return products
 
-users = []
-for i in range(0, N_USERS):
-    name = person_generator.full_name()
-    users.append(User(i, name))
+if __name__ == '__main__':
+    users = generate_users(N_USERS)
+    products = read_products(PRODUCTS_FILE, N_PRODUCTS)
 
-products = []
-valid_product_counter = 0
-with open('products.csv', encoding='latin-1') as file:
-    reader = csv.reader(file, delimiter=';')
-    header = next(reader)
-    for index, product_csv in enumerate(reader):
-        ean = product_csv[header.index('EANNummer')].strip()
-        name = product_csv[header.index('NaamRek')]
-        brand = product_csv[header.index('Merk')]
-        if not ean:
-            continue
-        products.append(Product(valid_product_counter, name, brand))
-        valid_product_counter += 1
-        if valid_product_counter >= N_PRODUCTS:
-            break
+    with open(USERS_FILE, 'w+') as file:
+        writer = csv.writer(file)
+        writer.writerow(['id', 'name', 'brand'])
+        for user in users:
+            writer.writerow([user.id, user.name, user.type.value])
 
-with open('users.gen.csv', 'w+') as file:
-    writer = csv.writer(file)
-    writer.writerow(['id', 'name', 'brand'])
-    for user in users:
-        writer.writerow([user.id, user.name, user.type.value])
-
-
-products = products[:N_PRODUCTS]
-with open('users_products.gen.csv', 'w+') as file:
-    writer = csv.writer(file)
-    writer.writerow(['user_id', 'product_id', 'orders'])
-    for user in users:
-        order = user.buy(products)
-        p_ids = set()
-        for product in order:
-            if product.id not in p_ids:
-                rating = order.count(product)
-                p_ids.add(product.id)
-                writer.writerow([user.id, product.id, rating])
+    products = products[:N_PRODUCTS]
+    with open(BUY_HISTORY_FILE, 'w+') as file:
+        writer = csv.writer(file)
+        writer.writerow(['user_id', 'product_id', 'orders'])
+        for user in users:
+            order = user.buy(products)
+            p_ids = set()
+            for product in order:
+                if product.id not in p_ids:
+                    rating = order.count(product)
+                    p_ids.add(product.id)
+                    writer.writerow([user.id, product.id, rating])
 
 
